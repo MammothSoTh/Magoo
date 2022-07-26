b7226a6fd2beb6da8966334ceca27f6f04dd8ebd
Your Name <you@example.com>
2022-07-26 17:19:13 +0700

    +navigation to page2 (click)

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:magoo101/main.dart';
+import 'package:magoo101/main.dart.bak';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
