a9b5f4d6523d982e642df2575c8ebbcb8a6d3bae
Your Name <you@example.com>
2022-08-01 20:23:22 +0700

    this may work?

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 
-void main() {
-  runApp(const MaterialApp(
-    title: 'Navigation Basics',
-    home: FirstRoute(),
-  ));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Indoor Navigation',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(child: MyHomePage()),
+      ),
+    );
+  }
 }
 
-class FirstRoute extends StatelessWidget {
-  const FirstRoute({super.key});
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var _textController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return new Scaffold(
       body: Center(
         child: MaterialButton(
           color: Colors.brown.shade500,
@@ -22,22 +38,30 @@ class FirstRoute extends StatelessWidget {
           onPressed: () {
             Future.delayed(Duration(milliseconds: 300), () {
               Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const SecondRoute()),
-              );
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => NextPage(value: _textController.text),
+                  ));
             });
           },
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Icon(
-                  Icons.mic,
-                  size: 170,
-                  color: Colors.white,
-                )
-              ],
-            ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Icon(
+                Icons.mic,
+                size: 170,
+                color: Colors.white,
+              ),
+              Text(
+                'Tap Anywhere!!',
+                style: TextStyle(color: Colors.white70),
+              ),
+              new ListTile(
+                title: new TextField(
+                  controller: _textController,
+                ),
+              ),
+            ],
           ),
         ),
       ),
@@ -45,36 +69,60 @@ class FirstRoute extends StatelessWidget {
   }
 }
 
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
+class NextPage extends StatefulWidget {
+  String value;
+  NextPage({Key? key, required this.value}) : super(key: key);
+
+  @override
+  _NextPageState createState() => new _NextPageState();
+}
+
+class _NextPageState extends State<NextPage> {
+  final FlutterTts flutterTts = FlutterTts();
+  final TextEditingController textEditingController = TextEditingController();
+
+  speak() async {
+    await flutterTts.setLanguage("en-US");
+    await flutterTts.setLanguage("th-TH");
+    await flutterTts.setPitch(1);
+    await flutterTts.speak("${widget.value}");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
+    return Container(
+      alignment: Alignment.center,
+      //child: Padding(
+      //padding: const EdgeInsets.all(32),
+      child: Center(
+        //mainAxisSize: MainAxisSize.min,
+        //children: <Widget>[
+        // TextFormField(
+        //  controller: textEditingController,
+        //),
         child: MaterialButton(
-          color: Colors.brown,
+          minWidth: 1200,
           height: double.infinity,
-          minWidth: 1000.0,
-          onPressed: () {
-            Future.delayed(Duration(milliseconds: 300), () {
-              Navigator.pop(context);
-            });
-          },
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const <Widget>[
-                Icon(
-                  Icons.graphic_eq,
-                  size: 170,
-                  color: Colors.white,
-                ),
-              ],
-            ),
+          onPressed: () => speak(),
+          color: Colors.brown,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: const <Widget>[
+              Icon(
+                Icons.graphic_eq,
+                size: 170,
+                color: Colors.white,
+              ),
+              Text(
+                'Tap Anywhere!!',
+                style: TextStyle(color: Colors.white70),
+              ),
+            ],
           ),
         ),
+        //],
       ),
+      //),
     );
   }
 }
